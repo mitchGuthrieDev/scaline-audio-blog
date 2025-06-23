caefb809f95630cbb7ad7e31ecd15d3b7d418aad
mitchguthriedev <mitchguthrie@scalineaudio.net>
2025-06-23 18:22:42 -0500

    data update

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -10,31 +10,38 @@ export interface Episode {
 
 export const episodes: Episode[] = [
   {
-    "slug": "01",
-    "title": "01   Antidance Theory [Metaprose]",
-    "description": "",
-    "audioUrl": "https://stream.scalineaudio.net/01%20-%20Antidance%20Theory%20%5BMetaprose%5D.mp3",
-    "length": "23:16"
-  },
-  {
     "slug": "02",
-    "title": "02   Pillars And Color [Metaprose]",
+    "title": "ACLSD [Metaprose]",
     "description": "",
-    "audioUrl": "https://stream.scalineaudio.net/02%20-%20Pillars%20and%20Color%20%5BMetaprose%5D.mp3",
-    "length": "29:37"
-  },
-  {
-    "slug": "04",
-    "title": "Metaprose   Generative SciFi Shit",
-    "description": "",
-    "audioUrl": "https://stream.scalineaudio.net/Metaprose%20-%20Generative%20SciFi%20Shit.mp3",
-    "length": "01:20"
+    "audioUrl": "https://stream.scalineaudio.net/ACLSD%20%5BMetaprose%5D.mp3",
+    "length": "40:09"
   },
   {
     "slug": "03",
-    "title": "Sky H1   Labyrinth (Metaprose Dub)",
+    "title": "Antidance Theory [Metaprose]",
     "description": "",
-    "audioUrl": "https://stream.scalineaudio.net/Sky%20H1%20-%20Labyrinth%20(Metaprose%20Dub).mp3",
-    "length": "03:04"
+    "audioUrl": "https://stream.scalineaudio.net/Antidance%20Theory%20%5BMetaprose%5D.mp3",
+    "length": "23:16"
+  },
+  {
+    "slug": "01",
+    "title": "Grief [Metaprose]",
+    "description": "",
+    "audioUrl": "https://stream.scalineaudio.net/Grief%20%5BMetaprose%5D.mp3",
+    "length": "33:22"
+  },
+  {
+    "slug": "04",
+    "title": "Pillars And Color [Metaprose]",
+    "description": "",
+    "audioUrl": "https://stream.scalineaudio.net/Pillars%20and%20Color%20%5BMetaprose%5D.mp3",
+    "length": "29:37"
+  },
+  {
+    "slug": "05",
+    "title": "The Visible And The Invisible [Metaprose]",
+    "description": "",
+    "audioUrl": "https://stream.scalineaudio.net/The%20Visible%20and%20the%20Invisible%20%5BMetaprose%5D.mp3",
+    "length": "27:58"
   }
 ];
