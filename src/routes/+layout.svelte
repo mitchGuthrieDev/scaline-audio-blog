3dbbf5e4be94cddbefc51a033031fa3d3d0badfd
mitchguthriedev <mitchguthrie@scalineaudio.net>
2025-06-23 12:13:14 -0500

    layout test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,23 @@
 <script>
   import "../app.css";
   import Sidebar from '$lib/components/Sidebar.svelte';
+  import { episodes } from '$lib/data';
 </script>
 
-<div class="page">
+<div class="layout">
   <aside class="sidebar">
     <Sidebar />
   </aside>
 
-  <main class="content">
+  <main class="main-content">
     <slot />
   </main>
+
+  <aside class="episode-list">
+    <ul>
+ 	 	{#each episodes as ep}
+    		<li><a href={`/episodes/${ep.slug}`}>{ep.title}</a></li>
+  		{/each}
+	</ul>
+  </aside>
 </div>
