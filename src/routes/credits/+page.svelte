ef075170358c84ca5e867b212ec8c4461e33cb24
mitchguthriedev <mitchguthrie@scalineaudio.net>
2025-06-23 12:59:35 -0500

    bug fix

---
+++ src/routes/credits/+page.svelte
@@ -0,0 +1,12 @@
+<!-- src/routes/credits/+page.svelte -->
+<script>
+  // any imports if needed
+</script>
+
+<pre>
+music for programming
+
+written & compiled by …  
+graphics by …  
+powered by sveltekit
+</pre>
